fd9d72d07ddebd261564f2d12d9de4665a52e2af
ElDavido81 <eldavido81@gmail.com>
2024-10-16 09:48:49 +0200

    Product & catalog classes

---
+++ Katalog/src/Catalog.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Catalog {
+
+    private List <Product> product;
+
+    public Catalog () {
+        product = new ArrayList<>();
+    }
+
+    public void AddProduct (Product product) {
+
+
+
+    }
+
+}
