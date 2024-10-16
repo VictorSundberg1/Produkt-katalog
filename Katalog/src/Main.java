b3bc51785ce0dd58e3384085014841b296f7664f
VictorSundberg1 <victor.sundberg1@hotmail.com>
2024-10-16 11:01:39 +0200

    Menu added

--- Katalog/src/Main.java
+++ Katalog/src/Main.java
@@ -1,13 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
-        Catalog catalog = new Catalog () ;
-
-        Product product1 = new Product ("bananer", 25) ;
-
-        catalog.AddProduct(product1);
-        catalog.ListProducts();
-
+        Menu menu = new Menu();
+        menu.displayMenu();
 
     }
 }
